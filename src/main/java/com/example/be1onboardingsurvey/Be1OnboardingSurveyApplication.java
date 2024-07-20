1356bacea2f045ad39f400795818ef00b6665af1
suebinmon <jg0100290@gmail.com>
2024-07-20 18:43:05 +0900

    init: survey 프로젝트 설정

---
+++ src/main/java/com/example/be1onboardingsurvey/Be1OnboardingSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.example.be1onboardingsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be1OnboardingSurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Be1OnboardingSurveyApplication.class, args);
+    }
+
+}
