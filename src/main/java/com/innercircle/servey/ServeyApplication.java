3a87b697a161356b66866060c334fab29ab49ea0
JaeHunJaeHun <jhl9838@gmail.com>
2024-07-20 18:30:26 +0900

    init : project setting

---
+++ src/main/java/com/innercircle/servey/ServeyApplication.java
@@ -0,0 +1,13 @@
+package com.innercircle.servey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innercircle/servey/ServeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innercircle.servey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
