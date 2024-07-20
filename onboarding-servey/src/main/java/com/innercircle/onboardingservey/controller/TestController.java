1cbe6ca5e8e875358f864362dd5be8836bd76257
최현웅 <121544753+hyunwoongchoi-29cm@users.noreply.github.com>
2024-07-20 17:10:25 +0900

    init : 스프링 부트 프로젝트 세팅 (#1)

---
+++ onboarding-servey/src/main/java/com/innercircle/onboardingservey/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.innercircle.onboardingservey.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
---
+++ onboarding-servey/src/test/java/com/innercircle/onboardingservey/OnboardingServeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innercircle.onboardingservey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingServeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
