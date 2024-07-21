dccece4fd1fb7122fe5ce0763397b018a9685e86
isohyeong <so-hyeong@naver.com>
2024-07-21 22:13:07 +0900

    first commit

---
+++ src/main/java/com/fastcampus/innercircle/survey_api/SurveyApiApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.innercircle.survey_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyApiApplication.class, args);
+	}
+
+}
