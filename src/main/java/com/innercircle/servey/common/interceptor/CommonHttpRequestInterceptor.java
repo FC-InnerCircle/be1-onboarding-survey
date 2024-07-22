4ecf7de17ed153ea6eb6fee8747a0c5b94d9aac2
JaeHunJaeHun <jhl9838@gmail.com>
2024-07-22 22:38:09 +0900

    feat : 인터넵터, 컨트롤러 어드바이저

---
+++ src/main/java/com/innercircle/servey/common/interceptor/CommonHttpRequestInterceptor.java
@@ -0,0 +1,29 @@
+package com.innercircle.servey.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class CommonHttpRequestInterceptor implements HandlerInterceptor {
+
+    public static final String HEADER_REQUEST_UUID_KEY = "x-request-id";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestEventId = request.getHeader(HEADER_REQUEST_UUID_KEY);
+        if (StringUtils.isEmpty(requestEventId)) {
+            requestEventId = UUID.randomUUID().toString();
+        }
+
+        MDC.put(HEADER_REQUEST_UUID_KEY, requestEventId);
+        return true;
+    }
+}
---
+++ src/main/java/com/innercircle/servey/interfaces/servey/SurveyController.java
@@ -0,0 +1,5 @@
+package com.innercircle.servey.interfaces.servey;
+
+public class SurveyController {
+
+}
