36c355d46efacc852d586c944d465235a39f675a
etff <wish2rich@gmail.com>
2024-07-23 20:45:30 +0900

    init project

---
+++ src/main/kotlin/org/example/survey/SurveyApplication.kt
@@ -0,0 +1,11 @@
+package org.example.survey
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SurveyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SurveyApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/survey/SurveyApplicationTests.kt
@@ -0,0 +1,11 @@
+package org.example.survey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SurveyApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
