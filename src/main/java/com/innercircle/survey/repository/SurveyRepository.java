814116c7701a1421d70860b9ee467c95e0c93c9c
Donghyeon0908 <donghyeonlee0908@gmail.com>
2024-07-23 23:50:38 +0900

    fix: surveryRepo

--- src/main/java/com/innercircle/survey/repository/SurveyRepository.java
+++ src/main/java/com/innercircle/survey/repository/SurveyRepository.java
@@ -1,2 +1,7 @@
-package com.innercircle.survey.repository;public class SurveyRepository {
+package com.innercircle.survey.repository;
+
+import com.innercircle.survey.domain.Survey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
 }
