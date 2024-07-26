cbc98d09b16cce8460cda5bd337f1eac0c77d1ce
wooyongjeong <wooyong.dev@gmail.com>
2024-07-26 22:39:14 +0900

    feat(create): Add SurveyController and createSurvey api

---
+++ src/main/java/com/innercircle/survey/controller/SurveyController.java
@@ -0,0 +1,27 @@
+package com.innercircle.survey.controller;
+
+import com.innercircle.survey.dto.request.SubmitResponseRequestDto;
+import com.innercircle.survey.dto.request.SurveyCreateRequestDto;
+import com.innercircle.survey.dto.request.SurveyUpdateRequestDto;
+import com.innercircle.survey.dto.response.SurveyResponseDto;
+import com.innercircle.survey.dto.response.SurveyResponseResponseDto;
+import com.innercircle.survey.dto.response.SurveyResponseResponseDtos;
+import com.innercircle.survey.service.SurveyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/surveys")
+@RestController
+public class SurveyController {
+    private final SurveyService surveyService;
+
+    @PostMapping
+    public ResponseEntity<SurveyResponseDto> createSurvey(
+            @RequestBody SurveyCreateRequestDto surveyCreateRequestDto
+    ) {
+        SurveyResponseDto responseDto = surveyService.createSurvey(surveyCreateRequestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+}
