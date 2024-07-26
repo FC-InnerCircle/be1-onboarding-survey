aa53ddc8bc290ad12c4677ff20a795f31a2f9ff3
ksh <tjdgus2319@gmail.com>
2024-07-27 03:22:34 +0900

    feat: 응답 엔티티 추가

--- survey/src/main/java/org/ksh/survey/entity/SurveyReplyItem.java
+++ survey/src/main/java/org/ksh/survey/entity/SurveyReplyItem.java
@@ -1,8 +1,15 @@
 package org.ksh.survey.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.ksh.survey.model.SurveyInputType;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class SurveyReplyItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,5 +21,21 @@ public class SurveyReplyItem {
     @ManyToOne(fetch = FetchType.LAZY)
     private SurveyReply surveyReply;
 
-    private String response;
+    private String itemReply;
+    private String itemName;
+    private SurveyInputType surveyInputType;
+    private boolean isRequired;
+
+    @Builder
+
+    public SurveyReplyItem(Long id, SurveyTemplateItem surveyTemplateItem, SurveyReply surveyReply, String itemReply, String itemName, SurveyInputType surveyInputType, boolean isRequired) {
+        this.id = id;
+        this.surveyTemplateItem = surveyTemplateItem;
+        this.surveyReply = surveyReply;
+        this.itemReply = itemReply;
+        this.itemName = itemName;
+        this.surveyInputType = surveyInputType;
+        this.isRequired = isRequired;
+    }
+
 }
