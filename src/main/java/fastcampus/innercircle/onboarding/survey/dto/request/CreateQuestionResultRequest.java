32d8ed12a626367dfa129f9d1cc0121a403494d8
chosungil <67699933+sung3441@users.noreply.github.com>
2024-07-27 17:36:05 +0900

    feat: create result api 유효성 추가
    - 필수 질문 답변 여부
    - 신청 폼과 답변 폼 형태 일치 검사
    - 답변 형태가 선택형일 때 option에 등록된지 검사 (TODO)
    - 답변 형태에 따른 답변 갯수 검사 (TODO)

--- src/main/java/fastcampus/innercircle/onboarding/survey/dto/request/CreateQuestionResultRequest.java
+++ src/main/java/fastcampus/innercircle/onboarding/survey/dto/request/CreateQuestionResultRequest.java
@@ -17,9 +17,19 @@ public record CreateQuestionResultRequest(
     }
 
     public SurveyResultDetail toEntity(final SurveyQuestion question) {
+        // 답변 내용과 신청 폼의 형태가 동일한지 검사
         if (!Objects.equals(this.questionId, question.getId())) { // TODO questions의 순서를 보장할 수 있게 position을 이용해서 정렬하는 로직을 추가하자, 혹은 조회 시 정렬
             throw new InvalidSurveyResultDataException("신청 폼과 제출한 데이터의 형식이 맞지 않습니다.");
         }
+
+        // 필수 질문 검사
+        if (question.isRequired() && values.isEmpty()) {
+            throw new InvalidSurveyResultDataException("필수 질문은 넘어갈 수 없습니다.");
+        }
+
+        // TODO value 값이 질문 option에 있었는지 검사
+
+        // TODO ResultType에 따른 답변 갯수 검사
 
         return SurveyResultDetail.builder()
                 .question(question)
